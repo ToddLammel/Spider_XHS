fb2c6713e948950974d5e8e3b174be437ba91dc2
Todd Lammel <52667240+ToddLammel@users.noreply.github.com>
2025-04-05 20:00:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,133 +1,20 @@
-import os
+import aiohttp
+import asyncio
+import json
+
 from loguru import logger
-from apis.pc_apis import XHS_Apis
-from xhs_utils.common_utils import init
-from xhs_utils.data_util import handle_note_info, download_note, save_to_xlsx
 
+from apis.pc_apis import XhsApi
 
-class Data_Spider():
-    def __init__(self):
-        self.xhs_apis = XHS_Apis()
-
-    def spider_note(self, note_url: str, cookies_str: str, proxies=None):
-        """
-        爬取一个笔记的信息
-        :param note_url:
-        :param cookies_str:
-        :return:
-        """
-        note_info = None
-        try:
-            success, msg, note_info = self.xhs_apis.get_note_info(note_url, cookies_str, proxies)
-            if success:
-                note_info = note_info['data']['items'][0]
-                note_info['url'] = note_url
-                note_info = handle_note_info(note_info)
-        except Exception as e:
-            success = False
-            msg = e
-        logger.info(f'爬取笔记信息 {note_url}: {success}, msg: {msg}')
-        return success, msg, note_info
-
-    def spider_some_note(self, notes: list, cookies_str: str, base_path: dict, save_choice: str, excel_name: str = '', proxies=None):
-        """
-        爬取一些笔记的信息
-        :param notes:
-        :param cookies_str:
-        :param base_path:
-        :return:
-        """
-        if (save_choice == 'all' or save_choice == 'excel') and excel_name == '':
-            raise ValueError('excel_name 不能为空')
-        note_list = []
-        for note_url in notes:
-            success, msg, note_info = self.spider_note(note_url, cookies_str, proxies)
-            if note_info is not None and success:
-                note_list.append(note_info)
-        for note_info in note_list:
-            if save_choice == 'all' or save_choice == 'media':
-                download_note(note_info, base_path['media'])
-        if save_choice == 'all' or save_choice == 'excel':
-            file_path = os.path.abspath(os.path.join(base_path['excel'], f'{excel_name}.xlsx'))
-            save_to_xlsx(note_list, file_path)
-
-
-    def spider_user_all_note(self, user_url: str, cookies_str: str, base_path: dict, save_choice: str, excel_name: str = '', proxies=None):
-        """
-        爬取一个用户的所有笔记
-        :param user_url:
-        :param cookies_str:
-        :param base_path:
-        :return:
-        """
-        note_list = []
-        try:
-            success, msg, all_note_info = self.xhs_apis.get_user_all_notes(user_url, cookies_str, proxies)
-            if success:
-                logger.info(f'用户 {user_url} 作品数量: {len(all_note_info)}')
-                for simple_note_info in all_note_info:
-                    note_url = f"https://www.xiaohongshu.com/explore/{simple_note_info['note_id']}?xsec_token={simple_note_info['xsec_token']}"
-                    note_list.append(note_url)
-            if save_choice == 'all' or save_choice == 'excel':
-                excel_name = user_url.split('/')[-1].split('?')[0]
-            self.spider_some_note(note_list, cookies_str, base_path, save_choice, excel_name, proxies)
-        except Exception as e:
-            success = False
-            msg = e
-        logger.info(f'爬取用户所有视频 {user_url}: {success}, msg: {msg}')
-        return note_list, success, msg
-
-    def spider_some_search_note(self, query: str, require_num: int, cookies_str: str, base_path: dict, save_choice: str, sort="general", note_type=0,  excel_name: str = '', proxies=None):
-        """
-            指定数量搜索笔记，设置排序方式和笔记类型和笔记数量
-            :param query 搜索的关键词
-            :param require_num 搜索的数量
-            :param cookies_str 你的cookies
-            :param base_path 保存路径
-            :param sort 排序方式 general:综合排序, time_descending:时间排序, popularity_descending:热度排序
-            :param note_type 笔记类型 0:全部, 1:视频, 2:图文
-            返回搜索的结果
-        """
-        note_list = []
-        try:
-            success, msg, notes = self.xhs_apis.search_some_note(query, require_num, cookies_str, sort, note_type, proxies)
-            if success:
-                notes = list(filter(lambda x: x['model_type'] == "note", notes))
-                logger.info(f'搜索关键词 {query} 笔记数量: {len(notes)}')
-                for note in notes:
-                    note_url = f"https://www.xiaohongshu.com/explore/{note['id']}?xsec_token={note['xsec_token']}"
-                    note_list.append(note_url)
-            if save_choice == 'all' or save_choice == 'excel':
-                excel_name = query
-            self.spider_some_note(note_list, cookies_str, base_path, save_choice, excel_name, proxies)
-        except Exception as e:
-            success = False
-            msg = e
-        logger.info(f'搜索关键词 {query} 笔记: {success}, msg: {msg}')
-        return note_list, success, msg
 
 if __name__ == '__main__':
-    """
-        此文件为爬虫的入口文件，可以直接运行
-        apis/pc_apis.py 为爬虫的api文件，包含小红书的全部数据接口，可以继续封装，感谢star和follow
-    """
-    cookies_str, base_path = init()
-    data_spider = Data_Spider()
-    # save_choice: all: 保存所有的信息, media: 保存视频和图片, excel: 保存到excel
-    # save_choice 为 excel 或者 all 时，excel_name 不能为空
-    # 1
-    notes = [
-        r'https://www.xiaohongshu.com/explore/67d7c713000000000900e391?xsec_token=AB1ACxbo5cevHxV_bWibTmK8R1DDz0NnAW1PbFZLABXtE=&xsec_source=pc_user',
-    ]
-    data_spider.spider_some_note(notes, cookies_str, base_path, 'all', 'test')
+    cookies = r'abRequestId=92fcc04b-656b-509f-8a1e-c3ac8e497f23; a1=1956beb135e5lv46l69faxnx5tbuna61jz5pwoev850000427689; webId=e78b2e096f0b8a5aab4ee52ffa364b7d; gid=yj2KDdDJdfVDyj2KDdDyquld2d21h4K1Kji02hYCIC29DU287I0KyE8884JWKYj8S8ySSW80; x-user-id-creator.xiaohongshu.com=5e24fb75000000000100509f; customerClientId=942161409810786; access-token-creator.xiaohongshu.com=customer.creator.AT-68c517486023646732033249ppejtee9xgofm0jp; galaxy_creator_session_id=gMM7NUHERAH9g2VJs89BsXjoRkhcUTGcu5FV; galaxy.creator.beaker.session.id=1742975704346011423609; xsecappid=xhs-pc-web; web_session=0400698f50b83d5600c98361d9354b8bb5902b; webBuild=4.62.1; acw_tc=0a4a661517438347833435632e23dabb3dc4399a0de12944f921ceec5e715e; unread={%22ub%22:%2267e5cd6a000000001a007a3b%22%2C%22ue%22:%2267e23cbe000000000603e2cc%22%2C%22uc%22:18}; loadts=1743835074786; websectiga=9730ffafd96f2d09dc024760e253af6ab1feb0002827740b95a255ddf6847fc8; sec_poison_id=a51d1970-f16e-4338-9763-15f76e46de95'
+    xhs_apis = XhsApi(cookies=cookies)
 
-    # 2
-    user_url = 'https://www.xiaohongshu.com/user/profile/67a332a2000000000d008358?xsec_token=ABTf9yz4cLHhTycIlksF0jOi1yIZgfcaQ6IXNNGdKJ8xg=&xsec_source=pc_feed'
-    data_spider.spider_user_all_note(user_url, cookies_str, base_path, 'all')
 
-    # 3
-    query = "榴莲"
-    query_num = 10
-    sort = "general"
-    note_type = 0
-    data_spider.spider_some_search_note(query, query_num, cookies_str, base_path, 'all', sort, note_type)
+    async def test():
+        success, msg, res_json = await xhs_apis.search_some_note('人口', require_num=10)
+        logger.info(f'登录结果 {json.dumps(res_json, ensure_ascii=False)}: {success}, msg: {msg}')
+
+
+    asyncio.run(test())
